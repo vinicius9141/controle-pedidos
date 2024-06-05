255c6cf3be001d156459c407ff5b5caa14fa95c2
vinicius <vinicius9141@gmail.com>
2024-06-05 18:37:00 -0300

    alteration on export modules

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { getFirestore, collection, getDocs, deleteDoc, doc, addDoc, query, where } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
 // Your web app's Firebase configuration
@@ -18,4 +17,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { auth, db, collection, getDocs, deleteDoc, doc, addDoc, query, where, onAuthStateChanged };
+export { auth, db, collection, getDocs, deleteDoc, doc, addDoc, query, where, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword };
